beb548947efc144bd104a0d60cfcb05939855319
Saadbasheer <saadbasheer0@gmail.com>
2024-04-20 23:44:43 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by{" "}
+          <a  target="_blank">
+            saadbasheer
+          </a>
+          . Intended for portfolio.
+        </p>
+      </small>
+
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
+}
---
+++ src/contexts/BookmarksContextProvider.tsx
@@ -0,0 +1,53 @@
+import { createContext, useEffect } from "react";
+import { useJobItems, useLocalStorage } from "../lib/hooks";
+import { JobItemExpanded } from "../lib/types";
+
+type BookmarksContext = {
+  bookmarkedIds: number[];
+  handleToggleBookmark: (id: number) => void;
+  bookmarkedJobItems: JobItemExpanded[];
+  isLoading: boolean;
+};
+
+export const BookmarksContext = createContext<BookmarksContext | null>(null);
+
+export default function BookmarksContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [bookmarkedIds, setBookmarkedIds] = useLocalStorage<number[]>(
+    "bookmarkedIds",
+    []
+  );
+
+  const { jobItems: bookmarkedJobItems, isLoading } =
+    useJobItems(bookmarkedIds);
+
+  const handleToggleBookmark = (id: number) => {
+    if (bookmarkedIds.includes(id)) {
+      setBookmarkedIds((prev) => prev.filter((item) => item !== id));
+    } else {
+      setBookmarkedIds((prev) => [...prev, id]);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("bookmarkedIds", JSON.stringify(bookmarkedIds));
+  }, [bookmarkedIds]);
+
+  return (
+    <BookmarksContext.Provider
+      value={{
+        bookmarkedIds,
+        handleToggleBookmark,
+        bookmarkedJobItems,
+        isLoading,
+      }}
+    >
+      {children}
+    </BookmarksContext.Provider>
+  );
+}
+
+
